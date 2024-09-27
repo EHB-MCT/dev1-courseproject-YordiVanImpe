ab388a880c88dd6cb7c4c2a955bdc31eab297970
YordiVanImpe <yordi.van.impe@student.ehb.be>
2024-09-27 16:41:25 +0200

    signature

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,31 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+DrawBackground();
+function DrawBackground() {
+	context.fillStyle = "black";
+	context.beginPath();
+	context.rect(0, 0, 300, 300);
+	context.stroke();
+	context.fill();
+}
 
-alert("Draw your space invader here");
+DrawLogo();
+function DrawLogo() {
+	context.fillStyle = "#94A7DF";
+
+	context.beginPath();
+	context.rect(20, 20, 50, 50);
+	context.rect(70, 20, 50, 50);
+	context.rect(170, 20, 50, 50);
+	context.rect(220, 20, 50, 50);
+	context.rect(70, 120, 50, 50);
+	context.rect(170, 120, 50, 50);
+	context.rect(70, 220, 50, 50);
+	context.rect(20, 220, 50, 50);
+	context.rect(170, 220, 50, 50);
+	context.rect(220, 220, 50, 50);
+	context.stroke();
+	context.fill();
+}
