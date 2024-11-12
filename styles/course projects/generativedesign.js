cbdfbce75f841588ba489262ee304cd1366d9812
YordiVanImpe <yordi.van.impe@student.ehb.be>
2024-11-12 22:03:05 +0100

    milestone 2

---
+++ styles/course projects/generativedesign.js
@@ -0,0 +1,20 @@
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+import * as Noise from "../scripts/noise.js";
+
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+let ofset - 10;
+
+perlin();
+playButton();
+// gebruik gemaakt van Peter Dick's Noise functie @erasmus brussel
+function peril() {
+for (let j = 0; j < 5; j++) {
+	
+}
+}
+
+
+
