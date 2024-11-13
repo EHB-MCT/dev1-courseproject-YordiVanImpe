eed7c9ef27c945fa00894d765ba934dc1b0c086b
YordiVanImpe <yordi.van.impe@student.ehb.be>
2024-11-13 16:50:54 +0100

    last update

--- styles/course projects/generativedesign.js
+++ styles/course projects/generativedesign.js
@@ -5,17 +5,18 @@ import * as Noise from "../scripts/noise.js";
 let width = window.innerWidth;
 let height = window.innerHeight;
 let offset = 10;
-
+context.fillStyle = "black";
+context.fillRect(0, 0, width, height);
 perlin();
 playButton();
 //
-// gebruik gemaakt van Peter Dick's Noise functie @erasmus brussel
+// gebruik gemaakt van Peter Dick's Noise functie @Erasmushogeschool Brussel
 function perlin() {
-	for (let j = 0; j < 5; j++) {
+	for (let j = 0; j < 7; j++) {
 		let r = Math.random() * 30;
 		let g = 10;
 		let b = 20;
-		let a = Math.random() * 15 + 10;
+		let a = Math.random() * 15 + 60;
 		context.strokeStyle = Utils.rgba(r, g, b, a);
 		context.lineWidth = 5;
 
@@ -31,7 +32,7 @@ function playButton() {
 	let x = width / 2;
 	let y = height / 2;
 	context.lineWidth = 5;
-	context.strokeStyle = Utils.hsla(Math.random() * 360, 80, 50, 100);
+	context.strokeStyle = Utils.hsla(Math.random() * 360, 80, 70, 100);
 	Utils.strokeCircle(x, height / 2, 100);
 	context.beginPath();
 	context.moveTo(x - 40 + offset, y - 40);
