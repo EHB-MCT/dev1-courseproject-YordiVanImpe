bc8d522b66a672421adaf192d77a89077f26985f
YordiVanImpe <yordi.van.impe@student.ehb.be>
2024-12-17 20:23:42 +0100

    update

--- styles/course projects/interactive design.js
+++ styles/course projects/interactive design.js
@@ -11,54 +11,36 @@ context.fillRect(0, 0, width, height);
 
 context.fillStyle = "black";
 context.fillRect(0, 0, width, height);
-
-DrawLogo(1400, 600, 20, 20); //  parameters voor positie en grootte
-update();
 playButton(width / 2, height / 2, 100, 5); // parameters voor positie, grootte en lijn dikte
+// Parameters adapted by chatGPT for the DEV1 course @ Erasmushogeschool Brussel
 
+update();
+DrawLogo();
 
 window.onmousemove = move;
 
 function playButton(x, y, radius, lineWidth) {
-    context.lineWidth = lineWidth;
-    context.strokeStyle = Utils.hsla(Math.random() * 360, 80, 70, 100);
-    Utils.strokeCircle(x, y, radius);
-    context.beginPath();
-    context.moveTo(x - 40 + offset, y - 40);
-    for (let i = 1; i < 3; i++) {
-        let s = i % 2;
-        let xPos = x + (-40 + 80 * s) + offset;
-        let yPos = y + (-40 + 40 * i);
-        context.lineTo(xPos, yPos);
-    }
-    context.closePath();
-    context.stroke();
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsla(Math.random() * 360, 80, 70, 100);
+	Utils.strokeCircle(x, y, radius);
+	context.beginPath();
+	context.moveTo(x - 40 + offset, y - 40);
+	for (let i = 1; i < 3; i++) {
+		let s = i % 2;
+		let xPos = x + (-40 + 80 * s) + offset;
+		let yPos = y + (-40 + 40 * i);
+		context.lineTo(xPos, yPos);
+	}
+	context.closePath();
+	context.stroke();
 }
-
 
 function update() {
-    playButton(width / 2, height / 2, 100, 5); 
-    requestAnimationFrame(update);
+	playButton(width / 2, height / 2, 100, 5);
+	DrawLogo();
+	requestAnimationFrame(update);
 }
 
-
-function DrawLogo() {
-	context.fillStyle = "#94A7DF";
-
-	context.beginPath();
-	context.rect(1400, 600, 20, 20);
-	context.rect(1420, 600, 20, 20);
-	context.rect(1465, 600, 20, 20);
-	context.rect(1485, 600, 20, 20);
-	context.rect(1420, 645, 20, 20);
-	context.rect(1465, 645, 20, 20);
-	context.rect(1400, 690, 20, 20);
-	context.rect(1420, 690, 20, 20);
-	context.rect(1465, 690, 20, 20);
-	context.rect(1485, 690, 20, 20);
-
-	context.fill();
-}
 /**
  *
  * @param {MouseEvent} e
@@ -82,4 +64,21 @@ function move(e) {
 			context.strokeRect(x, y, 1, 1);
 		}
 	}
+}
+function DrawLogo() {
+	context.fillStyle = "#94A7DF";
+
+	context.beginPath();
+	context.rect(1400, 600, 20, 20);
+	context.rect(1420, 600, 20, 20);
+	context.rect(1465, 600, 20, 20);
+	context.rect(1485, 600, 20, 20);
+	context.rect(1420, 645, 20, 20);
+	context.rect(1465, 645, 20, 20);
+	context.rect(1400, 690, 20, 20);
+	context.rect(1420, 690, 20, 20);
+	context.rect(1465, 690, 20, 20);
+	context.rect(1485, 690, 20, 20);
+
+	context.fill();
 }
